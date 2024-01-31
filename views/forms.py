a158a566e9c1d44d6b12e30af5e3f50b4e4b4c76
Shoua Thao <s.thao008@gmail.com>
2024-01-31 22:41:26 +0100

    both searches functional

--- views/forms.py
+++ views/forms.py
@@ -1,6 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, EmailField, PasswordField, BooleanField, TextAreaField
-from wtforms.validators import InputRequired, Email, Length, EqualTo
+from wtforms import (
+    Form,
+    StringField,
+    EmailField,
+    PasswordField,
+    BooleanField,
+    TextAreaField,
+    IntegerField,
+    SubmitField)
+from wtforms.validators import (
+    InputRequired,
+    Email,
+    Length,
+    EqualTo,
+    NumberRange,
+    Regexp)
 
 class LoginForm(FlaskForm):
     email = EmailField(
@@ -18,3 +32,55 @@ class LoginForm(FlaskForm):
             Length(min=8, max=32, message="Password between 8 and 32 characters")
         ]
     )
+    submit = SubmitField("Login")
+
+class SearchAccountForm(FlaskForm):
+    search_account_number = StringField(
+        "account number",
+        render_kw={
+            "size": 20,
+            "placeholder": "account number",
+            "autocomplete": "off",
+            "pattern": r'^\d+$',
+            "title": "Only numbers allowed for account number",
+            "required": True,
+        },
+        validators=[
+            InputRequired(),
+            Regexp(r'^\d+$', message="Only numbers allowed for account number")
+        ]
+    )
+    submit = SubmitField("Search")
+
+class SearchCustomerForm(FlaskForm):
+    search_first_name = StringField(
+        "first name",
+        id="search_first_name",
+        render_kw={"placeholder": "first name", "size": 20}, # you can even put css classes here
+        validators=[
+            InputRequired(),
+            Length(min=2)
+        ]
+    )
+
+    search_last_name = StringField(
+        "last name",
+        id="search_last_name",
+        render_kw={"placeholder": "last name", "size": 20}, # you can even put css classes here
+        validators=[
+            InputRequired(),
+            Length(min=2)
+        ]
+    )
+
+    search_city = StringField(
+        "city",
+        id="search_city",
+        render_kw={"placeholder": "city", "size": 20},
+        validators=(
+            InputRequired(),
+            Length(min=1)
+        )
+    )
+
+    submit = SubmitField("Search")
