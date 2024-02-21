ed94d3e949f5d4b53670b5b50a3cdbf66a2e7d1d
Shoua Thao <s.thao008@gmail.com>
2024-02-21 20:23:12 +0100

    deleted todo for console app

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ from views.transactions_pages import transactions_blueprint
 
 # TODO maybe simplify my prefixed forms
 # TODO work on frontend errors
-# TODO console app + mail
 
 def create_app():
     locale.setlocale(locale.LC_ALL, "sv_SE.UTF-8")
